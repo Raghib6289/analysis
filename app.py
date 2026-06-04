83bc93709c59b4f60f532b1cf54a7af9596db0a5
Md Raghib <kraghib123@gmail.com>
2026-06-04 22:48:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,6 @@ app.layout = html.Div(style={'fontFamily': 'Arial, sans-serif', 'padding': '20px
                     html.P([html.B("Status: "), html.Span(id="winner-status")])
                 ])
             ]),
-            
             # Right   Content
             html.Td(style={'width': '70%', 'verticalAlign': 'top', 'paddingLeft': '20px'}, children=[
                 html.H3("Projected 2026 Outcome"),
